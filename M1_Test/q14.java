94bcbdc8cb1b8e0cf6876518706e761dd0038a05
Rupika <rupika9124@gmail.com>
2026-03-10 14:33:35 +0530

    added new questions

--- M1_Test/q14.java
+++ M1_Test/q14.java
@@ -1,4 +1,17 @@
 package M1_Test;
 
+import java.util.Scanner;
+
 public class q14 {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int ld=n%10;
+            rev=rev*10+ld;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
 }
--- M1_Test/q2.java
+++ M1_Test/q2.java
@@ -1,4 +1,21 @@
 package M1_Test;
 
+import java.util.Scanner;
+
 public class q2 {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=0;
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                max=Math.max(max,arr[i-1]);
+            }
+        }
+        System.out.println(max);
+    }
 }
